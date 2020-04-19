54a038021d8fb357f5309bfbc24a1d918996f1ef
Filip Gabrys <el.filberto@gmail.com>
2020-04-19 19:43:04 +0200

    Kończenie zadania

--- Zadanie.py
+++ Zadanie.py
@@ -1,3 +1,14 @@
-# Zobaczymy czy teraz wyjdzie
-print("próba1")
-#ciekawe czy teraz wyjdzie
+lista = {
+    "piekarni": ["chleb", "bułki", "pączek"],
+    "warzywniaka": ["marchew", "seler", "rukole", "jabłko"]
+}
+print("Lista zakupów")
+z=0
+for a,b in lista.items():
+    a=a.capitalize()
+    b=[c.capitalize() for c in b]
+    print(f"Idę do {a} i kupuję tam następujące rzeczy: {b}.")
+    y=len(b)
+    print(y)
+    z=z+y
+print(f"W sumie kupuję {z} produktów.")
