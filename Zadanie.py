96e99e9fbb67e5db8a426c5db04c0c46ff6903f9
Filip Gabrys <el.filberto@gmail.com>
2020-04-19 19:47:43 +0200

    Wiadomość dla Mentora

--- Zadanie.py
+++ Zadanie.py
@@ -1,3 +1,4 @@
-# Zobaczymy czy teraz wyjdzie
-print("próba1")
-#ciekawe czy teraz wyjdzie
+print("Witam, czy o to chodziło w tym zadaniu?")
+#Ogólnie wszystko ujmując, jest to najgorzej napisany moduł. Łatwo się pogubić, nie wszystko jest jasne.
+#Drogą prób i błędów, czy szukanie poradników na youtubie moim zdaniem w tym module się nie sprawdza.
+#Za dużo czasu spedziłem na tym module.
